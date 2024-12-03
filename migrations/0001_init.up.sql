cdaf928165148f6f8a9c0c97694a9f16919fd1c0
Peter Holloway <peter.holloway@diamond.ac.uk>
2024-12-03 17:46:14 +0000

    Add DB constraint to prevent empty beamline name

--- migrations/0001_init.up.sql
+++ migrations/0001_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE beamline (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL CHECK (length(name) > 0),
     -- Default to 0 so scan numbering starts at 1
     scan_number INTEGER NOT NULL DEFAULT 0 CHECK (scan_number >= 0),
 
