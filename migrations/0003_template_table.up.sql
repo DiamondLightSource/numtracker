97e0295bc690d8330f487e8bb548513cb1b251a3
Peter Holloway <peter.holloway@diamond.ac.uk>
2025-02-13 16:04:59 +0000

    Working named template registration/retrieval
    Still can't do anything with the templates though.
    And they're not validated

--- migrations/0003_template_table.up.sql
+++ migrations/0003_template_table.up.sql
@@ -1,7 +1,19 @@
 -- Add new table for additional templates
-CREATE TABLE templates (
+CREATE TABLE template (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL CHECK (length(name) > 0),
-    template TEXT NOT NULL CHECK (length(name) > 0),
-    beamline INTEGER NOT NULL REFERENCES beamline(id) ON DELETE CASCADE ON UPDATE CASCADE
-)
+    name TEXT NOT NULL,
+    template TEXT NOT NULL,
+    beamline INTEGER NOT NULL REFERENCES beamline(id) ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT duplicate_names UNIQUE (name, beamline) ON CONFLICT REPLACE,
+
+    CONSTRAINT empty_template CHECK (length(template) > 0),
+    CONSTRAINT empty_name CHECK (length(name) > 0)
+);
+
+CREATE VIEW beamline_template (beamline, name, template) AS
+    SELECT
+        beamline.name, template.name, template.template
+    FROM beamline
+        JOIN template
+        ON beamline.id = template.beamline;
